72f4f034f5c09845dc3434fd58c691f09117abd1
joanpardogine <joanpardo@ginebro.cat>
2022-04-26 17:36:59 +0200

    pujat tot!

---
+++ sandbox.js
@@ -0,0 +1,30 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSHOK3w-PLxY0vNrM7ioSztmsca2pGQ2w",
+    authDomain: "pizzeria-f0b67.firebaseapp.com",
+    projectId: "pizzeria-f0b67",
+    storageBucket: "pizzeria-f0b67.appspot.com",
+    messagingSenderId: "644402833518",
+    appId: "1:644402833518:web:26bebfd8cd07c42ca59e7f"
+};
+
+// S'inicialitza l'aplicació de Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+// S'inicialitza Cloud Firestore i s'oté una referència al servei
+db.collection('recipes').get()
+    .then(snapshot => {
+        // console.log(snapshot.docs[0].data());
+        snapshot.forEach(doc => {
+            console.log(doc.data());
+        });
+    })
+    .catch(err => console.log(err));
+
+    // db.collection('recipes').get()
+    // .then(snapshot => {
+    //     // to do when data is recieved
+    //     console.log(snapshot);
+    // })
+    // .catch(err => console.log(err));
